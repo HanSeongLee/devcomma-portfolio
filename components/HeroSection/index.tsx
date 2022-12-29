c67d574e089f69f03a73f413610b50547557605e
HanSeongLee <jkhanseong@gmail.com>
2022-12-29 17:44:32 +0900

    ADD | HeroSection component on desktop

--- components/HeroSection/index.tsx
+++ components/HeroSection/index.tsx
@@ -13,30 +13,35 @@ const HeroSection: React.FC<IProps> = ({ className, ...props }) => {
         <section className={cn(styles.heroSection, className)}
                  {...props}
         >
-            <div className={styles.profileWrapper}>
-                <picture>
-                    <source srcSet={'/img/image-profile-mobile.webp'}
-                            media={'(max-width: 568px)'}
-                    />
-                    <source srcSet={'/img/image-profile-tablet.webp'}
-                            media={'(min-width: 568px)'}
-                    />
-                    <img className={styles.profile}
-                         src={'/img/image-profile-mobile.webp'}
-                    />
-                </picture>
-            </div>
+            <Container className={styles.container}>
+                <div className={styles.profileWrapper}>
+                    <picture>
+                        <source srcSet={'/img/image-profile-desktop.webp'}
+                                media={'(min-width: 992px)'}
+                        />
+                        <source srcSet={'/img/image-profile-tablet.webp'}
+                                media={'(min-width: 568px)'}
+                        />
+                        <source srcSet={'/img/image-profile-mobile.webp'} />
+                        <img className={styles.profile}
+                             src={'/img/image-profile-mobile.webp'}
+                        />
+                    </picture>
+                </div>
 
-            <Container className={styles.contentsBox}>
-                <h2 className={styles.title}>
-                    Nice to <br/>meet you! I’m <span className={styles.highlight}>Adam Keyes</span>.
-                </h2>
-                <p className={styles.description}>
-                    Based in the UK, I’m a front-end developer passionate about building accessible web apps that users love.
-                </p>
-                <Button>
-                    Contact Me
-                </Button>
+
+                <Container className={styles.contentsBox}>
+                    <h2 className={styles.title}>
+                        Nice to <br />meet you! I’m <span className={styles.highlight}>Adam Keyes</span>.
+                    </h2>
+                    <p className={styles.description}>
+                        Based in the UK, I’m a front-end developer passionate about building accessible web apps that
+                        users love.
+                    </p>
+                    <Button>
+                        Contact Me
+                    </Button>
+                </Container>
             </Container>
         </section>
     );
