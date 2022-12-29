f685315102deed11905bd2216244355e4580c76c
HanSeongLee <jkhanseong@gmail.com>
2022-12-29 18:01:47 +0900

    ADD | Main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,28 @@
 import type { NextPage } from 'next'
+import styles from './style.module.scss';
+import Header from 'components/Header';
+import HeroSection from 'components/HeroSection';
+import SkillSection from 'components/SkillSection';
+import ProjectSection from 'components/ProjectSection';
+import ContactSection from 'components/ContactSection';
 
 const Home: NextPage = () => {
-  return (
-      <>
-        <main>
-        </main>
-      </>
-  );
+    return (
+        <>
+            <main className={styles.main}>
+                <Header />
+                <div className={styles.container}>
+                    <HeroSection />
+                    <SkillSection />
+                    <ProjectSection />
+                    <div className={styles.contactSectionWrapper}>
+                        <ContactSection id={'contact'} />
+                        <Header className={styles.header} />
+                    </div>
+                </div>
+            </main>
+        </>
+    );
 }
 
 export default Home
