7c89aa1c4c317ed37ed30e979c13016c4bbd609d
HanSeongLee <jkhanseong@gmail.com>
2023-01-03 20:46:18 +0900

    FIX | Change names of AWS environment variables because of reservation from Vercel

--- lib/aws.client.ts
+++ lib/aws.client.ts
@@ -1,8 +1,8 @@
 import AWS from 'aws-sdk';
 
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID || '';
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY || '';
-const region = process.env.AWS_REGION;
+const accessKeyId = process.env.PROJECT_AWS_ACCESS_KEY_ID || '';
+const secretAccessKey = process.env.PROJECT_AWS_SECRET_ACCESS_KEY || '';
+const region = process.env.PROJECT_AWS_REGION;
 
 export const createAWSClient = () => {
     AWS.config.update({
