b4ffea33623326ebe9579096efc4faf4ea3082bd
HanSeongLee <jkhanseong@gmail.com>
2023-01-16 22:38:59 +0900

    ADD | Typing animation to title

--- components/HeroSection/index.tsx
+++ components/HeroSection/index.tsx
@@ -5,6 +5,7 @@ import cn from 'classnames';
 import ContactMeButton from 'components/ContactMeButton';
 import RingsPattern from 'public/img/pattern-rings.svg';
 import CirclePattern from 'public/img/pattern-circle.svg';
+import { TypeAnimation } from 'react-type-animation';
 
 interface IProps extends HTMLAttributes<HTMLDivElement> {
 
@@ -38,7 +39,41 @@ const HeroSection: React.FC<IProps> = ({ className, ...props }) => {
 
                 <Container className={styles.contentsBox}>
                     <h2 className={styles.title}>
-                        Nice to <br />meet you! I’m <span className={styles.highlight}>HanSeong Lee</span>.
+                        <TypeAnimation sequence={[
+                            'Nice to',
+                        ]}
+                                       cursor={false}
+                                       wrapper={'span'}
+                        /><br />
+                        <TypeAnimation sequence={[
+                            800,
+                            ' meet you!'
+                        ]}
+                                       cursor={false}
+                                       wrapper={'span'}
+                        />
+                        <TypeAnimation sequence={[
+                            2000,
+                            ` I'm `
+                        ]}
+                                       cursor={false}
+                                       wrapper={'span'}
+                        />
+                        <TypeAnimation sequence={[
+                            2500,
+                            `HanSeong Lee`
+                        ]}
+                                       cursor={false}
+                                       wrapper={'span'}
+                                       className={styles.highlight}
+                        />
+                        <TypeAnimation sequence={[
+                            3000,
+                            `.`
+                        ]}
+                                       cursor={true}
+                                       wrapper={'span'}
+                        />
                     </h2>
                     <p className={styles.description}>
                         Based in the South Korea, I’m a full-stack engineer passionate about Software Dev & I enjoy
