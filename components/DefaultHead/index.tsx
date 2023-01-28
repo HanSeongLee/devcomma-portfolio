2252ff5271868748d67ce4ba8e0f4a713bc4bfd2
HanSeongLee <jkhanseong@gmail.com>
2023-01-28 20:53:19 +0900

    ADD | Change title color

--- components/DefaultHead/index.tsx
+++ components/DefaultHead/index.tsx
@@ -69,7 +69,7 @@ const DefaultHead: React.FC<IProps> = ({ title, description, url, ogi }) => {
             <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
             <link rel="manifest" href="/manifest.json" />
-            <meta name="msapplication-TileColor" content="#ffffff" />
+            <meta name="msapplication-TileColor" content="#F8F8F8" />
             <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
             <meta name="theme-color" content="#ffffff" />
         </>
