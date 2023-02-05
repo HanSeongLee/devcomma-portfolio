d62ff337e24115d8fc8c3ead9c7fbcecbe16bd07
HanSeongLee <jkhanseong@gmail.com>
2023-02-05 22:40:29 +0900

    ADD | Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <>
+        <GoogleAnalytics trackPageViews />
+        <Component {...pageProps} />
+      </>
+  );
 }
 
 export default MyApp
