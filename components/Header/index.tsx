f89e50cff216a8c75bda968638b828c543ba2daf
HanSeongLee <jkhanseong@gmail.com>
2025-10-18 18:14:40 +0900

    feat: add navigation link to Calc & Tools

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,6 +4,7 @@ import cn from 'classnames';
 import Logo from 'components/Logo';
 import SocialContainer from 'containers/SocialContainer';
 import Container from 'components/Container';
+import Link from 'next/link';
 
 interface IProps extends HTMLAttributes<HTMLHeadElement> {
 
@@ -16,6 +17,13 @@ const Header: React.FC<IProps> = ({ className, ...props }) => {
         >
             <Container className={styles.container}>
                 <Logo />
+
+                <nav>
+                    <Link href={'https://tools.devcomma.com/'}>
+                        Calc & Tools
+                    </Link>
+                </nav>
+
                 <SocialContainer className={styles.socialContainer} />
             </Container>
         </header>
